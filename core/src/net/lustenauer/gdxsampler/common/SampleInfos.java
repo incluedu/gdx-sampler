6de99d1c7a7a7ed1d99063e4c0adf736bae80f2a
Patric Hollenstein <patric.hollenstein@lustenauer.net>
2018-01-13 21:39:50 +0100

    add SkinSample

--- core/src/net/lustenauer/gdxsampler/common/SampleInfos.java
+++ core/src/net/lustenauer/gdxsampler/common/SampleInfos.java
@@ -30,7 +30,8 @@ public class SampleInfos {
             TextureAtlasSample.SAMPLE_INFO,
             CustomActorSample.SAMPLE_INFO,
             ActionsSample.SAMPLE_INFO,
-            TableSample.SAMPLE_INFO
+            TableSample.SAMPLE_INFO,
+            SkinSample.SAMPLE_INFO
     );
 
     public static List<String> getSampleNames() {
